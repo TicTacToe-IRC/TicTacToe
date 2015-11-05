cf07c040910c9d8fcd1462c05f5daebdc5994848
dandray <draydan@hotmail.fr>
2015-11-05 09:02:53 +0100

    classe main

--- tictactoe/src/main/Java.java
+++ tictactoe/src/main/Java.java
@@ -1,12 +1,13 @@
 package main;
 
+import java.util.Scanner;
+
 import model.Couleur;
 import model.Joueur;
 import model.Plateau;
 
 public class Java {
 	public static void main(String[] args) {
-		System.out.println("Coucou !\n");
 		
 		Couleur bleu = new Couleur("bleu", 1, 1 ,1);
 		Couleur rouge = new Couleur("rouge", 2, 2 ,2);
@@ -15,25 +16,53 @@ public class Java {
 		Joueur joueur2 = new Joueur("PD", rouge, 2);
 		
 		System.out.println(joueur1.toString());
+		System.out.println(joueur2.toString());
 		
 		
 		Plateau plateau = new Plateau(4, 4, 4);
 		
-		plateau.afficherPlateau();
-		
+		//plateau.afficherPlateau();
+		/*
 		try {
 			plateau.placerPion(3, 3, joueur1.getId());
-			plateau.placerPion(3, 3, joueur2.getId());
-			plateau.placerPion(3, 3, joueur2.getId());
-			plateau.placerPion(3, 3, joueur2.getId());
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		plateau.afficherPlateau();
-		
-		
+
+		plateau.afficherPlateau();*/
+		int joueurCourant = 1;
+		int x=1, y=0, sz=1;
+		while(!plateau.gagner(x, y, z, joueurCourant)){
+			
+			System.out.println("Joueur " + joueurCourant +", placez un pion");
+			System.out.println("x: ");
+			Scanner sc = new Scanner(System.in);
+			x = sc.nextInt();
+			System.out.print("z: ");
+			z = sc.nextInt();
+			
+			try {
+				plateau.placerPion(x, z, joueurCourant);
+				System.out.println(x + ", " + (plateau.placementHauteur(x, z)-1) +", "+ z);
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			plateau.afficherPlateau();
+			if(joueurCourant == joueur1.getId()){
+				joueurCourant = joueur2.getId();
+			}
+			else{
+				joueurCourant = joueur1.getId();
+			}
+		}
+
+			System.out.println("Le joueur "+ joueurCourant + " a gagné");
+
 	}
+		
 }
--- tictactoe/src/model/Plateau.java
+++ tictactoe/src/model/Plateau.java
@@ -6,67 +6,86 @@ public class Plateau {
 	private int colonnes;
 	private int hauteur;
 	private int[][][] plateauJeu;
-	
-	
+
+
 	public Plateau(int l, int c, int h) {
 		super();
 		lignes = l;
 		colonnes = c;
 		hauteur = h;
 		plateauJeu = new int[l][c][h];
-		
+
 		for(int i=0; i< plateauJeu.length; i++){
 			for(int j=0; j< plateauJeu.length; j++)
 				for(int k=0; k< plateauJeu.length; k++)
 					plateauJeu[i][j][k] = 0;
 		}
 	}
-	
+
 	public void afficherPlateau(){
 		/* Lignes = horizontales
 		 * Colonnes = verticales */
-		for(int i=0; i< lignes; i++){
-			for(int j=0; j< colonnes; j++){
-				for(int k=0; k< hauteur; k++){
-					System.out.print(" | " + plateauJeu[i][j][k]);
+		for(int z=0; z< lignes; z++){
+			for(int y=0; y< hauteur; y++){
+				for(int x=0; x< colonnes; x++){
+					System.out.print(" | " + plateauJeu[x][y][z]);
 				}
-				System.out.println(" | hauteur" + j);
+				System.out.println(" | hauteur" + y);
 			}	
 			System.out.println("");
 		}
 		System.out.println("");
 	}
-	
-	public boolean placerPion(int x, int y, int idJoueur) throws Exception {
-		int z = indicePlacement(x, y);
-		if(z !=-1){
+
+	public boolean placerPion(int x, int z, int idJoueur) throws Exception {
+		int y = placementHauteur(x, z);
+		if(y !=-1){
 			//On place le pion du joueur "joueur"
-					plateauJeu[x][y][z] = idJoueur;
-					return true;
+			plateauJeu[x][y][z] = idJoueur;
+			return true;
 		}
 		throw new Exception();
 	}
-	
-	public int indicePlacement(int x, int y){
-		
+
+	public int placementHauteur(int x, int z){
+
 		/*Si on tente de mettre un pion hors du plateau OU que le batonnet sur 
 		lequel on tente de placer le pion est rempli */
-		if ((x<0 || x>3 || y<0 || y>3)){
-				return -1;
+		if ((x<0 || x>3 || z<0 || z>3)){
+			return -1;
 		}
 		else{
-			int z=0;
-			while(z<4 && plateauJeu[x][y][z]!=0){
-				z++;
+			int y=0;
+			while(y<4 && plateauJeu[x][y][z]!=0){
+				y++;
 			}
-			if(z<4){
-				return z;
+			if(y<4){
+				return y;
 			}
 			else{
 				return -1;
 			}
 		}
 	}
-	
-	
+
+	public int switchJoueur(int idJoueur){
+		if(idJoueur == 1){
+			idJoueur = 2;
+		}
+		else{
+			idJoueur = 1;
+		}
+		return idJoueur;
+	}
+
+	public boolean gagner(int x, int y, int z, int idJoueur){
+			if((plateauJeu[0][y][z]== idJoueur && plateauJeu[1][y][z]== idJoueur
+			&& plateauJeu[2][y][z]== idJoueur && plateauJeu[3][y][z]== idJoueur)
+			|| (plateauJeu[x][0][z]== idJoueur && plateauJeu[x][1][z]== idJoueur
+					&& plateauJeu[x][2][z]== idJoueur && plateauJeu[x][3][z]== idJoueur)){
+				return true;
+			}
+			else 
+				return false;	
+	}
 }
