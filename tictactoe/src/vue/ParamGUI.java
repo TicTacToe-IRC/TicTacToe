de095c2b850464f5e81bd552f6a2731afb3cfb4f
Maxime <Maxime@MouchouPC>
2015-11-06 09:33:15 +0100

    Checkbox j'ai gagné

--- tictactoe/src/vue/ParamGUI.java
+++ tictactoe/src/vue/ParamGUI.java
@@ -4,7 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,7 +15,7 @@ import javax.swing.JTextField;
 public class ParamGUI extends JFrame {
 	private JLabel jl, jl1, jl2;
 	private JTextField nom;
-	private JPanel jp1 = new JPanel(), jp2 = new JPanel(), jp3 = new JPanel();  
+	private JPanel jp1 = new JPanel(), jp2 = new JPanel(), jp3 = new JPanel() , jp4 = new JPanel();
 	private JTextField nom1 = new JTextField(), 
 			nom2 = new JTextField(); 
 	
@@ -69,6 +71,13 @@ public class ParamGUI extends JFrame {
 		jp3.add(valider);
 		this.getContentPane().add(jp3, BorderLayout.SOUTH);
 
+		/* Victoire */
+		
+		JCheckBox gagner = new JCheckBox ("Option : 'J'ai gagné'");
+		gagner.setPreferredSize(new Dimension(200, 30));
+		gagner.addActionListener(new Gagner());
+		jp4.add(gagner);
+		this.getContentPane().add(jp4, BorderLayout.NORTH);
 	}
 	
 	public class Valider implements ActionListener{
@@ -76,6 +85,13 @@ public class ParamGUI extends JFrame {
 			System.out.println(nom.getText());
 			jl1.setText("Vous avez saisi " + nom1.getText());
 			jl2.setText("Vous avez saisi " + nom2.getText());
+		}
+	}
+
+	
+	public class Gagner implements ActionListener{
+		public void actionPerformed(ActionEvent arg0) {
+			
 		}
 	}
 }
